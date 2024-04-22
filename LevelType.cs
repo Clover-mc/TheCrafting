c7e225c0783d6a3336d64afb29cb1a9f7be4faa4
Oleg <37769766+TheDearbear@users.noreply.github.com>
2024-04-22 22:01:40 +0300

    Remove whitespaces (bruh)

--- LevelType.cs
+++ LevelType.cs
@@ -5,9 +5,9 @@ namespace Minecraft;
 public class LevelType : Enumeration
 {
     public static LevelType Default     { get; } = new(0, "default");
-    
+
     public static LevelType Flat        { get; } = new(1, "flat");
-    
+
     public static LevelType LargeBiomes { get; } = new(2, "largeBiomes");
 
     public LevelType(int id, string type)
